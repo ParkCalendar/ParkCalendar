ac22ab53878a14e535339520b08868e316256b5f
Jeff Mark <git@jeffmark.com>
2022-12-27 11:23:38 -0800

    fix dates for vcal ics file

--- ical.sh
+++ ical.sh
@@ -27,30 +27,32 @@ __STOP
 
 for row in $(cat ${FILE} | jq -r '.operatingHours[] | .open, .close')
 do
-    # R=$(echo "${row}" | sed 's/-//g' | sed 's/://g')
-    R=${row}
+    R=$(echo "${row}" | sed 's/-//g' | sed 's/://g')
     if [[ "${START}" == "" ]]
     then
-        START=${R}
+        START=${row}
+        START_VCAL=${R}
         continue
     fi
 
-    END=${R}
+    END=${row}
+    END_VCAL=${R}
 
     OPEN=$(${DATECMD} +"%I:%M%p" -d "${START}" | sed 's/AM/a/' | sed 's/PM/p/' )
     CLOSE=$(${DATECMD} +"%I:%M%p" -d "${END}" | sed 's/AM/a/' | sed 's/PM/p/' )
 
     cat <<__STOP
 BEGIN:VEVENT
-UID:${START}@sixflags.com
-DTSTAMP:${START}
-DTSTART:${START}
-DTEND:${END}
+UID:${START_VCAL}@sixflags.com
+DTSTAMP:${START_VCAL}
+DTSTART:${START_VCAL}
+DTEND:${END_VCAL}
 SUMMARY:- ${CLOSE}
 END:VEVENT
 __STOP
 
     START=""
+    START_VCAL=""
 done
 
 cat <<__STOP
