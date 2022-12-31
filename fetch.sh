f5b514bfa4e889d34d38f64aea6bee112179d12a
Jeff Mark <git@jeffmark.com>
2022-12-31 06:37:30 -0800

    update fetch

--- fetch.sh
+++ fetch.sh
@@ -32,11 +32,14 @@ fi
 ./list.sh data/hours.json > data/hours.txt
 
 NOW=$(date +%m-%d-%Y)
+EXT=$(date +%Y%m%d)
+CHANGE_FILE=data/changelog.diff.${EXT}.txt
 
 echo "Diff times..."
-diff data/hours.txt data/current.txt > data/changelog.xx.diff.txt
+diff data/hours.txt data/current.txt > ${CHANGE_FILE}
+CHANGES_DIFF=$?
 
-if [[ "$?" == "0" && "${FORCE_UPDATE}" == "0" ]]
+if [[ "${CHANGES_DIFF}" == "0" && "${FORCE_UPDATE}" == "0" ]]
 then
     date
     echo "::notice::No Changes"
@@ -46,17 +49,24 @@ else
     MESSAGE="New Times"
     cp data/current.json data/hours.json
     cp data/current.txt data/hours.txt
+
+    echo "Generate ics (end) ..."
     ./ical.sh data/hours.json end > data/hours.end.ics
+
+    echo "Generate ics (subscribe) ..."
     ./ical.sh data/hours.json summary > data/hours.ics
 
-    echo "<div class='changelog-entry' data-change='${NOW}'><h2>${NOW}</h2><pre>" > data/changelog.xx.1.txt
-    echo "</pre></div>" > data/changelog.xx.2.txt
-    cat data/changelog.xx.1.txt data/changelog.xx.diff.txt data/changelog.xx.2.txt data/changelog.body.txt > data/changelog.xx.body.txt
-    mv data/changelog.xx.body.txt data/changelog.body.txt
-    cat data/changelog.head.html data/changelog.body.txt data/changelog.foot.html > data/changelog.html
-    rm data/changelog.xx.*
+    if [[ "${CHANGES_DIFF}" == "1" ]]
+    then
+        echo "<div class='changelog-entry' data-change='${NOW}'><h2>${NOW}</h2><pre>" > data/changelog.xx.1.txt
+        echo "</pre></div>" > data/changelog.xx.2.txt
+        cat data/changelog.xx.1.txt ${CHANGE_FILE} data/changelog.xx.2.txt data/changelog.body.txt > data/changelog.xx.body.txt
+        mv data/changelog.xx.body.txt data/changelog.body.txt
+        cat data/changelog.head.html data/changelog.body.txt data/changelog.foot.html > data/changelog.html
+        rm data/changelog.xx.*
+        git add data/changelog.*
+    fi
 
-    git add data/changelog.*
     git add data/current.*
     git add data/hours.*
 fi
