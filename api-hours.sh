50a03ce016b2bfd83ad19a55cbd9b05844fffd3f
Jeff Mark <git@jeffmark.com>
2023-01-05 07:47:34 -0800

    add hurricane harbor

--- api-hours.sh
+++ api-hours.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+##
+## Magic Mountain: 6
+## Hurricane Harbor: 11
 
 ## Hours
 curl https://api.sixflags.net/mobileapi/v1/park/6/hours
+
+## curl https://api.sixflags.net/mobileapi/v1/park/11/hours
 
 ## Ride Status
 #curl https://api.sixflags.net/mobileapi/v1/park/6/rideStatus
