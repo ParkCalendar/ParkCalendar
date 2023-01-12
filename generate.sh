f5bd75295be4d4862d9b3f25e4e588799f063519
Jeff Mark <git@jeffmark.com>
2023-01-11 19:20:03 -0800

    july/aug 2022

--- generate.sh
+++ generate.sh
@@ -2,11 +2,16 @@
 
 MONTH=$2
 YEAR=$1
+END=$3
+if [[ "${END}" == "" ]]
+then
+    END=17
+fi
 MAXDAY=31
 DAY=0
 while [[ "${DAY}" != "31" ]]
 do
     DAY=$(( DAY + 1 ))
     gdate "+%Y-%m-%dT10:30:00" -d "${YEAR}-${MONTH}-${DAY}"
-    gdate "+%Y-%m-%dT17:00:00" -d "${YEAR}-${MONTH}-${DAY}"
+    gdate "+%Y-%m-%dT${END}:00:00" -d "${YEAR}-${MONTH}-${DAY}"
 done
