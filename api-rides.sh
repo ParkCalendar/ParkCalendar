05f140a834c186f6787c19ea06344bce85ad1f80
Jeff Mark <git@jeffmark.com>
2023-01-16 13:58:01 -0800

    count

--- api-rides.sh
+++ api-rides.sh
@@ -6,8 +6,14 @@ RIDES_JSON="data/rides.json"
 ## Ride Status
 curl https://api.sixflags.net/mobileapi/v1/park/6/ride > ${RIDES_JSON}
 
+OPEN=$(( $(cat ${RIDES_JSON} | jq -r '.rides[] | select(.status | contains("Open")) | .name' | wc -l) + 0 ))
+CLOSED=$(( $(cat ${RIDES_JSON} | jq -r '.rides[] | select(.status | contains("Closed")) | .name' | wc -l) + 0 ))
+
 ##
 date +'%Y-%m-%d @ %H:%M %p'
+echo ""
+echo "Open Rides: ${OPEN}"
+echo "Closed: ${CLOSED}"
 echo ""
 echo "OPEN (Thrill)"
 echo "-------------"
