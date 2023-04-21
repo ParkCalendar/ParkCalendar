e985513b1fdb4c821ddb474597574edb072abff3
Jeff Mark <jffmrk@users.noreply.github.com>
2023-04-20 21:20:24 -0700

    thurs times for archive

--- data/all.sh
+++ data/all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cat parks.json | jq -r '.parks[] | [.state, .city, .name] | join(" - ")' | sort
+
+cat parks.json | jq -c '.parks[] | { "park_id": .parkId, "name": .name, "state": .state, "city": .city } '
