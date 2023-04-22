d15357c7d804f5eee657a1da8baaf75ec2cad75b
Jeff Mark <jffmrk@users.noreply.github.com>
2023-04-22 05:35:12 -0700

    fetch all

--- fetchAll.sh
+++ fetchAll.sh
@@ -29,32 +29,32 @@ echo "--"
 echo "-- Begin Fetch"
 echo "--"
 
-# ./fetch.sh ${ARG1} ${ARG2} 1
-# ./fetch.sh ${ARG1} ${ARG2} 2
-# ./fetch.sh ${ARG1} ${ARG2} 3
-# ./fetch.sh ${ARG1} ${ARG2} 5
-# ./fetch.sh ${ARG1} ${ARG2} 6
-# ./fetch.sh ${ARG1} ${ARG2} 7
-# ./fetch.sh ${ARG1} ${ARG2} 8
-# ./fetch.sh ${ARG1} ${ARG2} 10
+./fetch.sh ${ARG1} ${ARG2} 1
+./fetch.sh ${ARG1} ${ARG2} 2
+./fetch.sh ${ARG1} ${ARG2} 3
+./fetch.sh ${ARG1} ${ARG2} 5
+./fetch.sh ${ARG1} ${ARG2} 6
+./fetch.sh ${ARG1} ${ARG2} 7
+./fetch.sh ${ARG1} ${ARG2} 8
+./fetch.sh ${ARG1} ${ARG2} 10
 ./fetch.sh ${ARG1} ${ARG2} 11
-# ./fetch.sh ${ARG1} ${ARG2} 13
-# ./fetch.sh ${ARG1} ${ARG2} 14
-# ./fetch.sh ${ARG1} ${ARG2} 17
-# ./fetch.sh ${ARG1} ${ARG2} 20
-# ./fetch.sh ${ARG1} ${ARG2} 23
-# ./fetch.sh ${ARG1} ${ARG2} 24
-# ./fetch.sh ${ARG1} ${ARG2} 25
-# ./fetch.sh ${ARG1} ${ARG2} 28
-# ./fetch.sh ${ARG1} ${ARG2} 29
-# ./fetch.sh ${ARG1} ${ARG2} 32
-# ./fetch.sh ${ARG1} ${ARG2} 42
-# ./fetch.sh ${ARG1} ${ARG2} 43
-# ./fetch.sh ${ARG1} ${ARG2} 44
-# ./fetch.sh ${ARG1} ${ARG2} 45
-# ./fetch.sh ${ARG1} ${ARG2} 46
-# ./fetch.sh ${ARG1} ${ARG2} 47
-# ./fetch.sh ${ARG1} ${ARG2} 48
+./fetch.sh ${ARG1} ${ARG2} 13
+./fetch.sh ${ARG1} ${ARG2} 14
+./fetch.sh ${ARG1} ${ARG2} 17
+./fetch.sh ${ARG1} ${ARG2} 20
+./fetch.sh ${ARG1} ${ARG2} 23
+./fetch.sh ${ARG1} ${ARG2} 24
+./fetch.sh ${ARG1} ${ARG2} 25
+./fetch.sh ${ARG1} ${ARG2} 28
+./fetch.sh ${ARG1} ${ARG2} 29
+./fetch.sh ${ARG1} ${ARG2} 32
+./fetch.sh ${ARG1} ${ARG2} 42
+./fetch.sh ${ARG1} ${ARG2} 43
+./fetch.sh ${ARG1} ${ARG2} 44
+./fetch.sh ${ARG1} ${ARG2} 45
+./fetch.sh ${ARG1} ${ARG2} 46
+./fetch.sh ${ARG1} ${ARG2} 47
+./fetch.sh ${ARG1} ${ARG2} 48
 
 echo "--"
 echo "-- End Fetch"
